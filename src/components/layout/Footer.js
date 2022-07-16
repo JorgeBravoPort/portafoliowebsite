22faf22dd93bbde2b442a83866325e893b90566f
JorgeBravoPort <jorgebravoport@gmail.com>
2022-07-16 14:19:45 -0300

    fix

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = (props) => {
+    return (
+        <div>
+            <p>Copyright &copy; 2020, All rights reserved.</p>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = (props) => {
+    return (
+        <div>
+            <h1>Jorge Andrés Bravo Portillo</h1>
+        </div>
+    );
+}
+
+export default Header;
